4a555d1e100aa472a3bf7bfc66568112b7ab9e08
vinnyegon90 <vinnyegon90@gmail.com>
2024-01-16 09:23:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-  for (let i = 0; i < array.length; i++)
-  {
-    let firstNum = array[i];
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
 
-    let secondNum = target - firstNum;
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
 
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] === secondNum)
-       {
-        return true;
-      }
-    }
+    // .add adds the number to the Set
+    seenNumbers.add(number);
   }
   return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+  if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  consloe.log(""); 
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  }
+/*
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
+
 
 /* 
+
   Add your pseudocode here
 */
 
